a2033131fd1fcd64de095a0e910a7f44128446d2
HaeNoe <git@haenoe.party>
2024-07-08 09:49:35 +0200

    why

--- script.js
+++ script.js
@@ -15,29 +15,29 @@ function jsonToXml(json) {
 
 // Fetch JSON data from the API
 fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(jsonData => {
-        const xmlData = jsonToXml(jsonData);
+  .then(response => response.json())
+  .then(jsonData => {
+    const xmlData = jsonToXml(jsonData);
 
-        // Fetch XSLT file and transform XML to HTML
-        return fetch('style.xsl')
-            .then(response => response.text())
-            .then(xsltData => {
-                // Function to transform XML with XSLT
-                function transformXml(xmlString, xsltString) {
-                    
-                    const parser = new DOMParser();
-                    const xml = parser.parseFromString(xmlString, 'application/xml');
-                    const xslt = parser.parseFromString(xsltString, 'application/xml');
-                    const xsltProcessor = new XSLTProcessor();
-                    xsltProcessor.importStylesheet(xslt);
-                    const resultDocument = xsltProcessor.transformToFragment(xml, document);
-                    console.log(xmlString);
-                    return resultDocument;
-                }
+    // Fetch XSLT file and transform XML to HTML
+    return fetch('style.xsl')
+      .then(response => response.text())
+      .then(xsltData => {
+        // Function to transform XML with XSLT
+        function transformXml(xmlString, xsltString) {
 
-                // Display the transformed HTML in the tableContainer
-                document.getElementById('tableContainer').appendChild(transformXml(xmlData, xsltData));
-            });
-    })
-    .catch(error => console.error('Error:', error));
+          const parser = new DOMParser();
+          const xml = parser.parseFromString(xmlString, 'application/xml');
+          const xslt = parser.parseFromString(xsltString, 'application/xml');
+          const xsltProcessor = new XSLTProcessor();
+          xsltProcessor.importStylesheet(xslt);
+          const resultDocument = xsltProcessor.transformToFragment(xml, document);
+          console.log(xmlString);
+          return resultDocument;
+        }
+
+        const doc = transformXml(xmlData, xsltData);
+        document.getElementById('tableContainer').innerHTML = new XMLSerializer().serializeToString(doc);
+      });
+  })
+  .catch(error => console.error('Error:', error));
